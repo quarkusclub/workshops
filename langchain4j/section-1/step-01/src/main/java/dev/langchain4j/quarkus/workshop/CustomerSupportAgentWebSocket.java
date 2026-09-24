096d872eab4c541b9783522d40b43fcc1a0af3ef
Matheus Cruz <matheuscruz.dev@gmail.com>
2026-09-24 14:22:07 -0300

    Prepare section-1
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- langchain4j/section-1/step-01/src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgentWebSocket.java
+++ langchain4j/section-1/step-01/src/main/java/dev/langchain4j/quarkus/workshop/CustomerSupportAgentWebSocket.java
@@ -3,7 +3,6 @@ package dev.langchain4j.quarkus.workshop;
 import io.quarkus.websockets.next.OnOpen;
 import io.quarkus.websockets.next.OnTextMessage;
 import io.quarkus.websockets.next.WebSocket;
-import io.smallrye.mutiny.Multi;
 
 @WebSocket(path = "/customer-support-agent")
 public class CustomerSupportAgentWebSocket {
@@ -20,7 +19,7 @@ public class CustomerSupportAgentWebSocket {
     }
 
     @OnTextMessage
-    public Multi<String> onTextMessage(String message) {
+    public String onTextMessage(String message) {
         return customerSupportAgent.chat(message);
     }
 }
